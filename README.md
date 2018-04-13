97f3501631c54676b1a48a53ceefdd3872972b25
shreelimbkar <limbkarshree@gmail.com>
2018-04-13 17:30:32 +0530

    updated gulp file and docs
    updated gulp file and docs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# bootstrap4-starter-kit
+# BootStrap 4 Starter Kit
+
+A starter project with Bootstrap 4, Gulp and Browsersync.
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,8 @@
 const gulp = require('gulp');
-const browserSync = require('browser-sync');
+const browserSync = require('browser-sync').create();
 const sass = require('gulp-sass');
 
-// compile sass and inject into browser
+// Compile sass and inject into browser
 gulp.task('sass', function(){
     return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'])
     .pipe(sass())
@@ -12,31 +12,31 @@ gulp.task('sass', function(){
 
 // Move js files to src/js
 gulp.task('js', function(){
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 
-    'node_modules/popper.js/dist/umd/popper.min.js'])
-    .pipe(gulp.dest('src/js'));
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    .pipe(gulp.dest('src/js'))
+    .pipe(browserSync.stream());
 });
 
-// watch Sass and serve
+// Watch Sass and serve
 gulp.task('serve', ['sass'], function(){
     browserSync.init({
-        serve: './src'
+        server: './src'
     });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss'], ['sass']);
     gulp.watch('src/*.html').on('change', browserSync.reload);
-})
+});
 
 // Move fonts folder to src/fonts
 gulp.task('fonts', function(){
     return gulp.src('node_modules/font-awesome/fonts/*')
-    .pipe(gulp.dest('src/fonts'))
+    .pipe(gulp.dest('src/fonts'));
 });
 
 // Move font awesome to src/css
 gulp.task('fa', function(){
     return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-    .pipe(gulp.dest('src/css'))
-})
+    .pipe(gulp.dest('src/css'));
+});
 
 gulp.task('default', ['js', 'serve', 'fa', 'fonts']);
